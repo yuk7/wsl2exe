d50f51f0a0df81edd57c8f09e05d1dde0902b090
yuk7 <yukx00@gmail.com>
2020-12-10 23:07:40 +0900

    wsl2exe.c: wrote main

--- wsl2exe.c
+++ wsl2exe.c
@@ -6,10 +6,14 @@
  * http://opensource.org/licenses/mit-license.php
  */
 
+#include <stdbool.h>
 #include <stdio.h>
+#include <string.h>
 #include <wchar.h>
 #include "wsl2exe_common.h"
 #include "wsld.h"
+
+#define ARRAY_LENGTH(a) (sizeof(a)/sizeof(a[0]))
 
 int main()
 {
@@ -23,8 +27,27 @@ int main()
     wchar_t *distName;
     distName = WslGetDefaultDistroName();
 
-    wprintf(L"%S\n", modName);
-    wprintf(L"%S", distName);
-    
-    return 0;
+    size_t total = wcslen(modName);
+    for (int i = 1; i < wargc; i++) {
+		total++; //for space
+        total += wcslen(wargv[i]);
+	}
+
+    size_t totalSize = (sizeof(wchar_t) * (total + 1));
+    wchar_t *command = (wchar_t*)malloc(totalSize);
+    wcscat_s(command, totalSize ,modName);
+
+    for (int i = 1; i < wargc; i++) {
+        wcscat_s(command, totalSize ,L" ");
+        wcscat_s(command, totalSize ,wargv[i]);
+	}
+
+    HRESULT hr;
+    DWORD exitCode = 1;
+    hr = WslLaunchInteractive(distName, command, true, &exitCode);
+    if (hr == S_OK) {
+        return exitCode;
+    } else {
+        return hr;
+    }
 }
